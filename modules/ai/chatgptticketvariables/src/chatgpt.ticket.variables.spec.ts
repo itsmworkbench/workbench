24ce7c8fc4e5ee2326efedf88e91cf111880c8bf
Bzzman <eyupbarlas2134@gmail.com>
2024-03-11 11:16:19 +0100

    minor change

--- modules/ai/chatgptticketvariables/src/chatgpt.ticket.variables.spec.ts
+++ modules/ai/chatgptticketvariables/src/chatgpt.ticket.variables.spec.ts
@@ -2,7 +2,6 @@ import { chatgptTicketVariables, clientSecret } from "./chatgpt.ticket.variables
 
 describe ( 'ChatGPTTicketVariables', () => {
   it ( "should do cool stuff",async  () => {
-    console.log ( 'debug: clientSecret', clientSecret )
     expect (await  chatgptTicketVariables ( 'ticket' ) ).toEqual ( {a:1} )
 
   } )
