f3657e49935d25899fc51b316fa903edbecaaa5e
eyupbarlas <eyupbarlas2134@gmail.com>
2024-03-12 12:55:56 +0100

    enhancing accuracy + integration test fix

--- modules/ai/chatgptticketvariables/src/chatgpt.ticket.variables.integration.spec.ts
+++ modules/ai/chatgptticketvariables/src/chatgpt.ticket.variables.integration.spec.ts
@@ -3,7 +3,21 @@ import { chatgptTicketVariables } from "./chatgpt.ticket.variables";
 
 describe('ChatGPTTicketVariables', () => {
   test('should extract variables from ticket', async () => {
-    const sampleTicket = await chatgptTicketVariables('Ticket PA123 P4 Customer: a.customer@example.com Issue: I created a project (p-66a6) It is in the epx production by mistake. Please remove this project from the EPX production.');
+    const sampleTicket = await chatgptTicketVariables(`Ticket SR5542
+=============
+Ticket colour
+P4
+Customer: bob.grey@thecompany.co.
+
+Issue:
+* in the DSS production environment the colour of the button is wrong. It should be blue and is red.
+* The button is button id but-blue-123
+
+Action requested:
+* Please change the colour of the button
+
+Thanks
+Select * from item where item_id= ‘but-blue-123’ - 1 row returned`);
 
   });
 
--- modules/ai/chatgptticketvariables/src/chatgpt.ticket.variables.ts
+++ modules/ai/chatgptticketvariables/src/chatgpt.ticket.variables.ts
@@ -7,21 +7,67 @@ const openai = new OpenAI ( {
   apiKey: clientSecret,
 } );
 
-// Assuming TicketVariables is structured to store the APIs response.
-// The chatgptTicketVariables function is updated to simulate calling the ChatGPT API.
 export const chatgptTicketVariables: AiTicketVariablesFn = async ( ticket: string ): Promise<TicketVariables> => {
-  const systemPrompt = `You will be provided with a ITSM work ticket, and your task is to extract important variables from it. Return these variables only as in key value format.`;
+  const systemPrompt = `You will be provided with a ITSM work ticket, and your task is to extract important variables from it. Return these variables only as in JSON format.`;
 
-  const chatCompletion = await openai.chat.completions.create ( {
+  const chatCompletion = await openai.chat.completions.create ({
     messages: [
       { role: 'system', content: systemPrompt },
+      {
+        role: 'user',
+        content: `Ticket PA123
+============
+P4
+Customer: a.customer@example.com
+
+Issue:
+ * I created a project (P-6666)
+ * It is in the EPX acceptance by mistake.
+
+Action requested:
+* Please delete this.
+
+Thanks`
+      },
+      {
+        role: 'assistant',
+        content: ` 
+{"ticketId": "PA123",
+"projectId": "P-6666",
+"System: "EPX",
+"Environment": "acceptance"}`
+      },
+      {
+        role: 'user',
+        content: `Ticket price44
+==============
+P4
+Customer: a.customer@example.com
+
+Issue:
+* In the EPX the discombobulator (item code 1234-44) has an incorrect price.
+* The price is currently 55.55.
+* The price should be 44.44.
+
+Please update the price of the discombobulator`
+      },
+      {
+        role: 'assistant',
+        content: `
+{"ticketId": "price44",
+"Customer": "a.customer@example.com",
+"itemId": "1234-44",
+"itemName": "discombobulator"}`
+      },
       { role: 'user', content: ticket },
     ],
     model: 'gpt-3.5-turbo',
-  } );
+    temperature: 0,
+  });
 
   // Assuming chatCompletion.choices contains the formatted string with variables.
   const variablesString = chatCompletion.choices[ 0 ].message.content;
+  console.log(variablesString);
 
   return JSON.parse ( variablesString );
 };
