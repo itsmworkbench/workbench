61618122f7d039b4cbf88b3dac882c3a80b3d20f
phil <phil.rice@validoc.org>
2024-03-24 12:32:01 +0100

    removing knowledge articles projects

--- modules/domain/defaultdomains/src/organisation.default.domains.ts
+++ modules/domain/defaultdomains/src/organisation.default.domains.ts
@@ -3,7 +3,6 @@ import { YamlCapability } from "@itsmworkbench/yaml";
 import { operatorNameSpaceDetails } from "@itsmworkbench/operator";
 import { ticketNamespaceDetails } from "@itsmworkbench/tickets";
 import { NameAnd } from "@laoban/utils";
-import { knowledgeArticleNameSpaceDetails } from "@itsmworkbench/actions";
 
 import { ticketEventsNameSpaceDetails } from "@itsmworkbench/ticketevents";
 import { ticketTypeNamespaceDetails } from "@itsmworkbench/tickettype";
@@ -12,7 +11,6 @@ import { ticketTypeNamespaceDetails } from "@itsmworkbench/tickettype";
 //Even though this is 'for git' it can be safely used by the 'for api' version
 export function defaultNameSpaceDetails ( yaml: YamlCapability, env: NameAnd<string> ): NameAnd<NameSpaceDetailsForGit> {
   return {
-    ks: knowledgeArticleNameSpaceDetails ( yaml ),
     ka: ticketTypeNamespaceDetails ( yaml ),
     ticket: ticketNamespaceDetails (),
     operator: operatorNameSpaceDetails ( yaml, env ),
