3295f8b29bad752f9fd5e9ba59a17ef051f24197
phil <phil.rice@validoc.org>
2024-04-10 02:18:34 +0200

    Adding schema suggestion

--- modules/capabilities/ai/ai/src/ae.suggest.tx.ts
+++ modules/capabilities/ai/ai/src/ae.suggest.tx.ts
@@ -7,3 +7,10 @@ export type AISuggestTxInput = {
 export type AISuggestTxOutput = string
 
 export type AISuggestTx = ( suggest: AISuggestTxInput ) => Promise<AISuggestTxOutput>
+
+export type SuggestJsonSChemaInput = {
+  input: any
+}
+export type SuggestJsonSChemaOutput = string
+
+export type AISuggestJsonSchema = ( suggest: SuggestJsonSChemaInput ) => Promise<SuggestJsonSChemaOutput>
